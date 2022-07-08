1992246c73c6f0027d871e1d12d95a260db523be
Craig Brinkman <craigfrancis77@gmail.com>
2022-07-08 16:05:56 -0600

    first commit

---
+++ hellenportfolio/src/main/java/com/marketing/hellenportfolio/hellenportfolio/HellenportfolioApplication.java
@@ -0,0 +1,13 @@
+package com.marketing.hellenportfolio.hellenportfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellenportfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellenportfolioApplication.class, args);
+	}
+
+}
---
+++ hellenportfolio/src/main/java/com/marketing/hellenportfolio/hellenportfolio/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.marketing.hellenportfolio.hellenportfolio;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(HellenportfolioApplication.class);
+	}
+
+}
